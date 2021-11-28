ad1a4c06b530d5140a7493785c288835549ff9e9
Jeff Hollan <jeffhollan@outlook.com>
2021-11-28 15:05:28 -0800

    scaffolding

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,19 @@ import styles from './Header.module.scss'
 
 export default function Header({ children }) {
     return (
-        <div className={styles.Jeff}>{children}</div>
+        <header>
+            <div className="container">
+                <div className={styles.logo}>
+                    <i className="fa-solid fa-leaf"></i>&nbsp;&nbsp;Evergreen Plants
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="shop.html">Shop</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
